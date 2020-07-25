36d01ed6efd3616559fe3f86fc47264148f5f5c6
moehtet <laxus3331@gmail.com>
2020-07-26 02:27:56 +0630

    finished circular progress view

--- app/src/main/java/com/mhst/custom_view_assignment/views/CircularProgressView.kt
+++ app/src/main/java/com/mhst/custom_view_assignment/views/CircularProgressView.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.ReceiverCallNotAllowedException
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.core.content.withStyledAttributes
 import com.mhst.custom_view_assignment.R
+import java.math.BigDecimal
 
 /**
  * Created by Moe Htet on 25,July,2020
@@ -30,13 +32,21 @@ class CircularProgressView @JvmOverloads constructor(
         val paint = Paint()
         paint.apply {
             strokeWidth = 5f
-            setStyle(Paint.Style.STROKE)
+            style = Paint.Style.STROKE
             color = Color.BLUE
             textSize = 40f
         }
+        val originPaint = Paint().apply {
+            strokeWidth = 5f
+            style = Paint.Style.STROKE
+            color = Color.DKGRAY
+        }
+        val  y = (height/2) - ((originPaint.descent() + originPaint.ascent()) / 2)
         //path.addArc(recfF,270f,180f)
-        canvas?.drawText("${progress}%",(width/2).toFloat(),(height/2).toFloat(),paint)
-        canvas?.drawArc(recfF,270f,progress.toFloat(),true,paint)
+        canvas?.drawText("${progress}%",(width/2).toFloat(),y,paint)
+        canvas?.drawArc(recfF,270f,360f,false,originPaint)
+        val calculated = BigDecimal(progress).divide(BigDecimal(100)).multiply(BigDecimal(360))
+        canvas?.drawArc(recfF,270f,calculated.toFloat(),false,paint)
     }
 
 }
