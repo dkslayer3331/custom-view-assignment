b2e333a4d850e54b00342995796496285fc5de4d
moehtet <laxus3331@gmail.com>
2020-07-26 03:35:16 +0630

    wip

--- app/src/main/java/com/mhst/custom_view_assignment/activities/CreateTaskActivity.kt
+++ app/src/main/java/com/mhst/custom_view_assignment/activities/CreateTaskActivity.kt
@@ -4,18 +4,34 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.mhst.custom_view_assignment.R
+import kotlinx.android.synthetic.main.activity_create_task.*
 
 class CreateTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_task)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+
     }
 
     companion object{
         fun onNewIntent(context: Context) : Intent{
             return Intent(context,
                 CreateTaskActivity::class.java)
+
         }
 
     }
